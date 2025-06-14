20fddb9fd459cea60dd36d0f80b33ce9965f6773
emmanuel-ebiwari <ebiwariemma@gmail.com>
2025-06-14 19:27:06 +0100

    Add API URL routing and implement listing and booking views

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Listing, Booking
+from .serializers import ListingSerializer, BookingSerializer
 
 # Create your views here.
+class ListingView(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing listing instances.
+    """
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+
+    def get_queryset(self):
+        return self.queryset
+class BookingView(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing booking instances.
+    """
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+    def get_queryset(self):
+        return self.queryset
